b68034bb1c05ca1bd06f51ec1105628292e595ee
Lee <leer5501@gmail.com>
2016-01-14 10:55:33 -0600

    Upload PHP file
    more testing

---
+++ contact.php
@@ -0,0 +1,1 @@
+<?php  $to = "leer5501@gmail.com";  $subject = "Contact Me";  $email = $_REQUEST['email'] ;  $message = $_REQUEST['message'] ;  $headers = "From: $email";  $sent = mail($to, $subject, $message, $headers) ;  if($sent)  {print "Your mail was sent successfully"; } else  {print "We encountered an error sending your mail"; } ?>
